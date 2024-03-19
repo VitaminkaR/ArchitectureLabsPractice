5f0193198c8ab1ee7396aeb852c30be2b07aab29
VladimirR <rvitaminka20@gmail.com>
2024-03-19 20:10:17 +0300

    first lab add input for fib an fact

--- ArchitectureLabsPractice/Program.cs
+++ ArchitectureLabsPractice/Program.cs
@@ -4,14 +4,23 @@
     {
         static private void Main(string[] args)
         {
+            string input = "";
+
             Console.WriteLine("Первое задание (вывести аргументы):");
             PrintArgs.Print(args);
             Console.WriteLine("Второе задание (високсоные года 1900-2000):");
             LeapYears.WriteLeapYears(1900, 2000);
             Console.WriteLine("Третье задание (числа Фибоначчи):");
-            FibonacciNumbers.PrintFinonacciNumbers(15);
+            input = Console.ReadLine();
+            if (input == "")
+                input = "15";
+            FibonacciNumbers.PrintFinonacciNumbers(int.Parse(input));
             Console.WriteLine("Четвертое задание (факториал):");
-            Factorial.Fact(5);
+            input = "";
+            input = Console.ReadLine();
+            if (input == "")
+                input = "5";
+            Factorial.Fact(int.Parse(input));
             Console.WriteLine("Пятое задание (простые числа через решето Эратосфена):");
             PrimeNumbers.PrintPrimeNumbers(100);
         }
